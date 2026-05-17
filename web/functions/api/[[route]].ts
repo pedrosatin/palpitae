efa4d1e58dda6156eda9a7c944b5012597e07895
Pedro Satin <pedro.satin@saudebliss.com.br>
2026-05-17 20:58:57 -0300

    redirect over function

--- web/functions/api/[[route]].ts
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context: { request: Request }) {
-  const url = new URL(context.request.url)
-  url.hostname = 'api.palpitae.com.br'
-  url.port = ''
-  url.protocol = 'https:'
-
-  const request = new Request(url.toString(), context.request)
-  return fetch(request)
-}
